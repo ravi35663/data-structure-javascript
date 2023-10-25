d57db2c02485f22f3ee736f83cec75154f201f76
ravi <ravikr845430@gmail.com>
2023-10-25 10:12:13 +0530

    BFS implemented for BST and also DFS-Pre-order implemented

--- Trees/BinarySearchTree/index.js
+++ Trees/BinarySearchTree/index.js
@@ -47,16 +47,48 @@ class BinarySearchTree{
         }
         return null;
     }
+    // Traverse Tree (Breadth first search)
+    BFS(){
+        let node = this.root;
+        let queue = [];
+        let result = [];
+        // add item in queue
+        queue.push(node);
+        // console.log("Queue is ",queue);
+        while(queue.length){
+            node = queue.shift();//Dequeue item
+            result.push(node.value);
+            if(node.left){
+                queue.push(node.left);//enqueue item
+            }
+            if(node.right){
+                queue.push(node.right)//enqueue item
+            }
+        }
+        return result;
+    }
+    // Depth first search (Pre-Order)
+    dfsPreOrder(){
+        let result = [];
+        let current = this.root;
+        function traverse(node){
+            result.push(node.value);
+            if(node.left) traverse(node.left); // first left values will be added to the result array.
+            if(node.right) traverse(node.right); // then right values will be added to the result array.
+        }
+        traverse(current);
+        return result;
+    }
 }
-
+const arr = [10,9,1,5,10,20,19,25,27];
+// const arr = [15,10,8,12,20,16,25];
 const bst = new BinarySearchTree();
-bst.insert(10)
-bst.insert(9)
-bst.insert(20)
-bst.insert(1)
-bst.insert(10)
-bst.insert(5)
-console.log("bst.find(1) ",bst.find(1))
-console.log("bst.find(10) ",bst.find(10))
-console.log("bst.find(16) ",bst.find(16))
+arr.forEach(item=>{
+    bst.insert(item);
+})
+console.log("Breadth first search :",bst.BFS());
+console.log("Depth first search pre-order: ",bst.dfsPreOrder());
+// console.log("bst.find(1) ",bst.find(1))
+// console.log("bst.find(10) ",bst.find(10))
+// console.log("bst.find(16) ",bst.find(16))
 
