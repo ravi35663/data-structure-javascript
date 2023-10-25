426764549da66fd42767b3dec68a29cb0c46a0b2
ravi <ravikr845430@gmail.com>
2023-10-25 15:05:13 +0530

    tree traversal implemented

--- Trees/BinarySearchTree/index.js
+++ Trees/BinarySearchTree/index.js
@@ -72,9 +72,42 @@ class BinarySearchTree{
         let result = [];
         let current = this.root;
         function traverse(node){
-            result.push(node.value);
-            if(node.left) traverse(node.left); // first left values will be added to the result array.
-            if(node.right) traverse(node.right); // then right values will be added to the result array.
+            if(node){
+                result.push(node.value);
+                if(node.left) traverse(node.left); // first left values will be added to the result array.
+                if(node.right) traverse(node.right); // then right values will be added to the result array.
+            }
+        }
+        traverse(current);
+        return result;
+    }
+    // Depth first search (Post-order)
+    dfsPostOrder(){
+        const result = [];
+        const current = this.root;
+        function traverse(node){
+            if(node){
+                if(node.left){
+                    traverse(node.left);
+                }
+                if(node.right){
+                    traverse(node.right);
+                }
+                result.push(node.value);
+            }
+        }
+        traverse(current);
+        return result;
+    }
+    dfsInOrder(){
+        const result = [];
+        const current = this.root;
+        function traverse(node){
+            if(node){
+                if(node.left) traverse(node.left);
+                result.push(node.value);
+                if(node.right) traverse(node.right);
+            }
         }
         traverse(current);
         return result;
@@ -82,12 +115,16 @@ class BinarySearchTree{
 }
 const arr = [10,9,1,5,10,20,19,25,27];
 // const arr = [15,10,8,12,20,16,25];
+// const arr = [50,31,27,51,53,30,54,80,75,35,20,51];
+// const arr = [...'qwertyuiopasdfghjklzxcvbnm']
 const bst = new BinarySearchTree();
 arr.forEach(item=>{
     bst.insert(item);
 })
-console.log("Breadth first search :",bst.BFS());
+// console.log("Breadth first search :",bst.BFS());
 console.log("Depth first search pre-order: ",bst.dfsPreOrder());
+console.log("Depth first search post-order: ",bst.dfsPostOrder());
+console.log("Depth first search in-order: ",bst.dfsInOrder());
 // console.log("bst.find(1) ",bst.find(1))
 // console.log("bst.find(10) ",bst.find(10))
 // console.log("bst.find(16) ",bst.find(16))
