e6cc30f0ee0e16861676a5fae2cda027427e3ae5
ravi kumar <ravi.kumar07@telusinternational.com>
2024-09-09 13:31:21 +0530

    learnt how to extract maximum value from max-heap

--- Heap/MaxBinaryHeap/index.js
+++ Heap/MaxBinaryHeap/index.js
@@ -13,6 +13,31 @@
         Parent = Floor((n-1)/2)
 */
 
+/*
+===> Extract Maximum value:
+    ->  The procedure for deleting the root from the heap (effiently extracting maximum-element from 
+        the max heap or extracting the minimum element from the min-heap) and restoring the 
+        properties is called down-heap (AKA bubble-down, shift-down,extract min-max).
+*/ 
+
+/*
+    Steps to extract maximum element from the heap:
+    1) Swap the first value in the values (arr) with the last one.
+    2) Pop from the values properties, so you can return the value at the end.
+    3) Have the new-root "sinke-down" to the correct spot
+        -> Your parent index start at 0 (root index)
+        -> find the index of your left child ( 2*index + 1) (Make sure it is not out of bound)
+        -> If left child is greater than parent then swap with parent
+        -> Find the rightChild (2 * index + 2) (Make sure it is not out of bound)
+        -> Check right child is greater than parent swap with parent.
+        -> Always swapped with largest (Right or Left) with parent.
+        -> The children you swapped become parent.
+        -> Keep looping and swap untill neither child is larger than the parent
+    4) return the Old root
+
+
+*/
+
 // Implementation of max binaryHeap:
 class MaxBinaryHeap{
     constructor(){
@@ -25,20 +50,46 @@ class MaxBinaryHeap{
     }
     // Remove latest element
     extractMax(){
-        let parent_index = 0;
-        while(true){
-            const left_child_index = (2 * parent_index + 1)
-            const right_child_index = 2 * (parent_index + 1);
-            if(this.values[parent_index] > this.values[left_child_index] && this.values[parent_index] > this.values[right_child_index_child_index]){
-                if(this.values[left_child_index] > this.values[right_child_index_child_index]){
-                    [this.values[parent_index],this.values[left_child_index]] = [this.values[left_child_index],this.values[parent_index]]
-                    parent_index = left_child_index;
-                }else{
-                    [this.values[parent_index],this.values[right_child_index]] = [this.values[right_child_index],this.values[parent_index]]
-                    parent_index = right_child_index;
-                }
-            }else{
+        // [this.values[0],this.values[this.values.length-1]] = [this.values[this.values.length-1],this.values[0]]
+        const end = this.values.pop();
+        const max = this.values[0];
+        if( this.values.length > 0){
+            this.values[0] = end;
+            // Bubble-Down
+            this.bubbleDown()    
+        }
+        return max;
+    }
 
+    bubbleDown(){
+        let idx = 0;
+        let parent = this.values[idx];
+        let len = this.values.length;
+        while(true){
+            let leftIndex = (2 * idx + 1);
+            let rightIndex = (2 * idx + 2);
+            let swap = null;
+            // Check the left child value with the parent
+            if(leftIndex < len){
+                if(this.values[leftIndex] > parent){
+                    swap = leftIndex;
+                }
+            }
+            // Check the right child value with parent
+            if(rightIndex < len){
+                if( 
+                    ( swap === null && this.values[rightIndex] > parent ) || 
+                    ( swap !== null && this.values[rightIndex] > this.values[leftIndex] )
+                ){
+                    swap = rightIndex;
+                }
+            }
+            if(swap === null){
+                break;
+            }else{
+                // idx = swap;
+                [this.values[idx], this.values[swap]] = [this.values[swap], this.values[idx]];
+                idx = swap;
             }
         }
     }
@@ -65,6 +116,13 @@ const max_binary_heap = new MaxBinaryHeap();
 max_binary_heap.insert(10)
 max_binary_heap.insert(20)
 max_binary_heap.insert(30)
-max_binary_heap.insert(40)
+max_binary_heap.insert(42)
+max_binary_heap.insert(39)
+max_binary_heap.insert(35)
+max_binary_heap.insert(50)
+max_binary_heap.viewMaxBinaryHeap();
+max_binary_heap.extractMax();
+max_binary_heap.extractMax();
 max_binary_heap.viewMaxBinaryHeap();
 // Completed this code
+// [50, 39, 42, 10, 30, 20, 35]
