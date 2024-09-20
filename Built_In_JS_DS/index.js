d93335b95993fdc2b42a05295a65b8ec0ed943d3
ravi kumar <ravi.kumar07@telusinternational.com>
2024-09-20 20:51:58 +0530

    learning dsa

--- Built_In_JS_DS/index.js
+++ Built_In_JS_DS/index.js
@@ -45,10 +45,75 @@
     -> shift is used to remove first element in the array and it will return that first element.
     -> shift modify original array.
     -> if array is empty then shift will return 'undefined'.
-    const arr  = [1,2,3,4,5]
-    ele = arr.shift();
-    console.log("Element is : ",ele)// 1
-    console.log("Arr is ",arr) // [2,3,4,5]
-    items = [];
-    ele = items.shift() // undefined
+        const arr  = [1,2,3,4,5]
+        ele = arr.shift();
+        console.log("Element is : ",ele)// 1
+        console.log("Arr is ",arr) // [2,3,4,5]
+        items = [];
+        ele = items.shift() // undefined
 */
+
+/*
+===> unshift:
+    ->  In JavaScript, the unshift() method adds one or more elements to the beginning of an array and returns 
+        the new length of the array.
+    -> unshift modify the array.
+        const arr1 = [5,6]
+        arr1.unshift(1,2,3,4)
+        console.log("Array is : ",arr1); // [ 1, 2, 3, 4, 5, 6 ]
+*/
+
+/*
+===> splice
+    ->  The splice() method in JavaScript is a versatile array method that allows you to add, remove, or 
+        replace elements from an array. 
+        It modifies the original array and returns an array of the deleted elements, if any.
+*/
+
+/*
+==> Syntax :
+    arr.splice(start,deleteCount,item1,item2,item3,........)
+
+    start: the index at which arr start changing
+    deleteCount: the number of elements to remove from the array. if set 0, no elements will be removed.
+    item1,item2,item3:  the elements to add to the array starting from the 'start'. 
+                        if omitted then no element will be added.
+*/
+
+// Common Use Cases:
+
+// Remove elements: You can use splice() to remove elements from the array.
+const arr = ['a','b','c','d'];
+arr.splice(1,2);
+console.log("Array is: ",arr);
+
+
+// Remove elements: You can use splice() to remove elements from the array.
+// const arr = ['a','b','c','d'];
+// arr.splice(1, 2); // Removes 2 elements starting from index 1
+// console.log(arr); // Output: ['a', 'd']
+
+// Add elements: You can add elements by setting deleteCount to 0.
+// const arr = ['a','b'];
+// arr.splice(2,0,'c','d')
+// console.log("Array is: ",arr); //[ 'a', 'b', 'c', 'd' ]
+// arr.splice(1,0,'c','d')
+// console.log("Array is: ",arr); //[ 'a', 'c', 'd', 'b' ]
+
+// Replace elements: You can also replace elements by specifying both deleteCount and new items.
+// const arr = ['a','x','y','d'];
+// arr.splice(1,2,'b','c') // Replaces 'x' and 'y' with 'b' and 'c'
+// console.log(arr); //[ 'a', 'b', 'c', 'd' ]
+
+
+// Removing items
+// let fruits = ['apple', 'banana', 'cherry', 'date'];
+// let removeItems = fruits.splice(1,2)
+// console.log("Removed items",removeItems); //[ 'banana', 'cherry' ]
+// console.log("fruits: ",fruits) // [ 'apple', 'date' ]
+
+
+//Adding items:
+fruits = ['apple','date'];
+fruits.splice(2,0,'cherry','banana');
+console.log("Fruits: ",fruits) // [ 'apple', 'date', 'cherry', 'banana' ]
--- test.js
+++ test.js
@@ -1,80 +1,30 @@
-/*
-    Problem Description: Sort Strings
 
-    Given a list of 'n' strings S0, S1, S2, ..., Sn-1, each consisting of digits and spaces, the number of 
-    spaces is the same for each entry. 
-    The goal is to implement a variation of a sort command. 
-    1) None of the strings contains consecutive spaces. 
-    2) Also, no string starts with a space nor ends with it. 
-    3) Spaces are used to divide strings into columns, which can be used as keys in comparisons.
+// Remove elements: You can use splice() to remove elements from the array.
+// const arr = ['a','b','c','d'];
+// arr.splice(1, 2); // Removes 2 elements starting from index 1
+// console.log(arr); // Output: ['a', 'd']
 
-    The program has to support the required parameters:
+// Add elements: You can add elements by setting deleteCount to 0.
+// const arr = ['a','b'];
+// arr.splice(2,0,'c','d')
+// console.log("Array is: ",arr); //[ 'a', 'b', 'c', 'd' ]
+// arr.splice(1,0,'c','d')
+// console.log("Array is: ",arr); //[ 'a', 'c', 'd', 'b' ]
 
-    1) key: Integer denoting the column used as a key in comparisons. The left-most column is denoted by 1.
-    2) reversed: Boolean variable indicating whether to reverse the result of comparisons.
-    3) comparison-type: Either lexicographic or numeric.
-    
-    Lexicographic means that we use lexicographical order, where for example ('122' < '13').
-    Numeric means that we compare the strings by their numerical values, so (13 < 122).
-    If the comparison type is 'numeric' and the numeric values of keys of Si and Sj are equal for i < j, 
-    then Si is considered strictly smaller than Sj because it comes first.
-
-    Sample Input:
-    3
-    92 022  ==> s1
-    82 12   ==> s2
-    77 13   ==> s3
-    2 false numeric   ==> key -> 2 (column), reverse -> false , comparison-type -> numberic
+// Replace elements: You can also replace elements by specifying both deleteCount and new items.
+// const arr = ['a','x','y','d'];
+// arr.splice(1,2,'b','c') // Replaces 'x' and 'y' with 'b' and 'c'
+// console.log(arr); //[ 'a', 'b', 'c', 'd' ]
 
 
-    Sample Output:
-    82 12
-    77 13
-    92 022
-*/
+// Removing items
+// let fruits = ['apple', 'banana', 'cherry', 'date'];
+// let removeItems = fruits.splice(1,2)
+// console.log("Removed items",removeItems); //[ 'banana', 'cherry' ]
+// console.log("fruits: ",fruits) // [ 'apple', 'date' ]
 
-function stringKeySort(n,str,key,reverse,comparison){
-    let obj = {};
-    str = str.split('\n');
-    let key_arr = [];
-    str.forEach((item,index)=>{
-        const splited_item = item.split(' ');
-        obj[index] = splited_item;
-        key_arr.push([index,splited_item[key-1]]);
-    });
-    if(comparison === 'numeric'){
-        key_arr.sort((a,b)=>{
-            Number(a[1]) - Number(b[1]);
-        })
-    }else{
-        key_arr.sort((a,b)=>{
-            if(a[1] > b[1]){
-                return 1
-            }else{
-                return -1
-            }
-        })
-    }
-    if(reverse){
-        for(let i=0;i<Math.floor(key_arr.length/2);i++){
-            [key_arr[i],key_arr[key_arr.length-1-i]] = [key_arr[key_arr.length-1-i],key_arr[i]]
-        }
-    }
-    str = '';
-    for(let i=0;i<key_arr.length;i++){
-        const index = key_arr[i][0];
-        str += obj[index].join(' ') + '\n'; 
-    }
-    return str;
-}
-// [ [ 0, '12' ], [ 1, '13' ], [ 2, '022' ] ]
 
-const n = 3;
-const str = "82 12\n77 13\n92 022";
-const key = 2 // column
-const reverse = true;
-const comparison = 'numeric';
-
-const result = stringKeySort(n,str,key,reverse,comparison);
-console.log(result);
-
+//Adding items:
+fruits = ['apple','date'];
+fruits.splice(2,0,'cherry','banana');
+console.log("Fruits: ",fruits) // [ 'apple', 'date', 'cherry', 'banana' ]
