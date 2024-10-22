51f3bf337b4c0a694d353f353aff7b124afa3c70
ravi kumar <ravi.kumar07@telusinternational.com>
2024-10-22 22:02:19 +0530

    learning dsa

---
+++ revision.js
@@ -0,0 +1,18 @@
+/*
+===> Revise Topics Serial wise:
+    1) (Basic Approaches), Terminologies and FlowChart, Built-In Js DS and BigO, DS - Introduction  
+    2) Array ==> inprogress
+    3) String
+    4) Sliding window
+    5) Bit-Manipulation
+    6) Searching
+    7) Sorting 
+    8) Recursion 
+    9) Linked List 
+    10) Stack
+    11) Queue
+    12) Binary Search Tree and Tree - Traversal
+    13) Heap
+    14) HashTable
+    15) Graph
+*/
--- test.js
+++ test.js
@@ -1,52 +1,5 @@
-/*
-💡  Square Roots
+// Shift will modify the array and returns the value
 
-    Given an integer N and an integer P, you need to find the square root of number N up to P places. 
-    Do it without using a library function.
-
-    Input Array: N = 10, P = 3
-
-    Output:
-    3.162
-*/
-
-function squareRoot(n,p){
-    const integer = getInteger(n);
-    console.log("Integer is: ",integer);
-    let k = 0.1;
-    let v = integer;
-    while(p){
-        while(v * v <= n){
-            v  = v + k
-        }
-        v = v - k;
-        k = k / 10.0;
-        p--;
-    }
-    return v;
-}
-
-// Get the integer part:
-function getInteger(n){
-    let start = 1;
-    let end = n;
-    let root = -1;
-    while(start <=end){
-        let mid = Math.floor((start+end)/2);
-        let s = mid * mid
-        if(s === n){
-            return mid;
-        }else if(s < n){
-            start = mid+1
-            root = mid;
-        }else{
-            end = mid-1
-        }
-    }
-    return root;
-}
-
-const n = 10;
-const p = 3;
-
-squareRoot(n,p);
+const arr = [..."1234567890"]
+const a = arr.shift();
+console.log("arr",arr,a);
